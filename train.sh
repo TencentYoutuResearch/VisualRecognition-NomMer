7b4f54a8d4e0ca1996f730c5bbd074bac195b41e
clarkjiang(蒋兴华) <clarkjiang@tencent.com>
2022-04-01 17:01:57 +0800

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python -m torch.distributed.launch \
---nproc_per_node 8 --master_port 12345 main.py \
+--nproc_per_node 1 --master_port 12345 main.py \
 --cfg configs/nommer_small_attn.yaml --data-path <imagenet-path> \
 --amp-opt-level O1 --accumulation-steps 1 --batch-size 128 --output ./output
